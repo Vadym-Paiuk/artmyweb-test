01fbf1926aad14b838bb6d51bc25b56cf399beb1
Vadym Paiuk <vadympaiuk@gmail.com>
2023-10-18 19:50:30 +0300

    add discount logic for woo cart

--- wp-content/plugins/woo-custom-discounts/woo-custom-discounts.php
+++ wp-content/plugins/woo-custom-discounts/woo-custom-discounts.php
@@ -45,7 +45,7 @@
 				'taxonomy'           => 'product_cat',
 				'orderby'            => 'name',
 				'hierarchical'       => 1,
-				'depth'              => 1,
+				'depth'              => 0,
 				'show_count'         => 0,
 				'hide_empty'         => 0,
 				'selected'           => get_option( $option_name )
@@ -80,7 +80,7 @@
 				'taxonomy'           => 'product_cat',
 				'orderby'            => 'name',
 				'hierarchical'       => 1,
-				'depth'              => 1,
+				'depth'              => 0,
 				'show_count'         => 0,
 				'hide_empty'         => 0,
 				'selected'           => get_option( $option_name )
@@ -114,4 +114,105 @@
 	
 	function select_callback( $args ){
 		wp_dropdown_categories( $args );
+	}
+	
+	add_action( 'woocommerce_cart_contents', 'add_free_product_selection_row' );
+	function add_free_product_selection_row(){
+		$free_cat = get_option( 'free_cat' );
+		$free_cat = get_term($free_cat, 'product_cat');
+		$args = [
+			'category' => [$free_cat->slug],
+			'limit' => -1,
+			'status' => 'publish'
+		];
+		$products = wc_get_products( $args );
+		
+		echo '<tr><td></td><td>PRODUCT FREE</td><td><select name="free_product">';
+		echo '<option value="0">Select Product</option>';
+		foreach ($products as $product) {
+			echo '<option value="' . $product->get_id() . '">' . $product->get_name() . '</option>';
+		}
+		echo '</select></td></tr>';
+	}
+	
+	add_action('woocommerce_before_calculate_totals', 'custom_cart_recalculate');
+	function custom_cart_recalculate() {
+		if (is_admin() && !defined('DOING_AJAX')) {
+			return;
+		}
+		
+		$cart = WC()->cart;
+		$discount_cat = get_option( 'discount_cat' );
+		$numbers_input = get_option( 'numbers_input' );
+		$count_product_in_discount_cat = 0;
+		$has_free_product = false;
+		
+		foreach ($cart->get_cart() as $cart_item_key => $cart_item) {
+			$term_list_ids = wp_get_post_terms( $cart_item['product_id'], 'product_cat', ['fields' => 'ids'] );
+			
+			if (in_array( $discount_cat, $term_list_ids )){
+				$count_product_in_discount_cat += $cart_item['quantity'];
+			}
+			
+			if( isset( $cart_item['free_product'] ) && $cart_item['free_product'] === true ){
+				$has_free_product = true;
+				$free_product = $cart_item['data'];
+				$free_product_key = $cart_item_key;
+				$free_product->set_price(0);
+			}
+		}
+		
+		if ( $numbers_input > $count_product_in_discount_cat && $has_free_product ){
+			$cart->remove_cart_item($free_product_key);
+			$has_free_product = false;
+		}
+		
+		if ( $numbers_input > $count_product_in_discount_cat ){
+			remove_action( 'woocommerce_cart_contents', 'add_free_product_selection_row' );
+		}
+		
+		if( $has_free_product ){
+			remove_action( 'woocommerce_cart_contents', 'add_free_product_selection_row' );
+		}
+	}
+	
+	add_action('woocommerce_update_cart_action_cart_updated', 'custom_update_cart');
+	function custom_update_cart(){
+		if( empty( $_POST['free_product'] ) ){
+			return;
+		}
+		
+		$product_id = (int)$_POST['free_product'];
+		$quantity = 1;
+		$cart_item_data = [
+			'free_product' => true
+		];
+
+		if (WC()->cart) {
+			WC()->cart->add_to_cart($product_id, $quantity, 0, [] ,$cart_item_data);
+		}
+	}
+	
+	add_filter('woocommerce_cart_item_quantity', 'remove_ability_change_quantity', 10, 2);
+	function remove_ability_change_quantity($item_quantity, $cart_item_key) {
+		$cart = WC()->cart;
+		$cart_item = $cart->get_cart_item($cart_item_key);
+		
+		if( isset( $cart_item['free_product'] ) && $cart_item['free_product'] === true ){
+			$cart->set_quantity($cart_item_key);
+			return $cart_item['quantity'];
+		}
+		
+		return $item_quantity;
+	}
+	
+	add_action('wp_enqueue_scripts', 'add_custom_inline_script');
+	function add_custom_inline_script() {
+		$script ='
+            $(document).on("change", "[name=free_product]", function(){
+				$( ".woocommerce-cart-form" ).find( ":input[name=update_cart]" ).prop( "disabled", false );
+            });
+        ';
+		
+		wp_add_inline_script('jquery', 'jQuery(document).ready(function($) { ' . $script . ' });');
 	}
